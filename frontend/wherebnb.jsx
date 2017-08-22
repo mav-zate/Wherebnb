abf8cb61d2d473b4995a4daf9666bb33147986e3
mav-zate <maverick.alzate@gmail.com>
2017-08-22 11:37:08 -0400

    Add api_session_util methods

--- frontend/wherebnb.jsx
+++ frontend/wherebnb.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { signup, login, logout } from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Let there be light</h1>, root);
 });
 
+window.signup = signup;
+window.login = login;
+window.logout = logout;
 
-// $.ajax({
-//   method: 'POST',
-//   url: 'api/session',
-//   data: {
-//     user: {
-//       email: 'maverick.alzate@gmail.com',
-//       password: '7723646'
-//     }
-//   }
-// })
+$.ajax({
+  email: 'calvin@gmail.com',
+  first_name: 'Calvin',
+  last_name: 'Jee',
+  password: 'password',
+  language: 'English',
+  country: 'United States',
+  
+})

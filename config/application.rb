ef47aa8f81e46a2ac37cb3ee82dd8d7835e39f16
mav-zate <maverick.alzate@gmail.com>
2017-08-25 17:24:13 -0400

    Add :s3_region line to application.rb for heroku push

--- config/application.rb
+++ config/application.rb
@@ -13,11 +13,12 @@ module Wherebnb
     # -- all .rb files in that directory are automatically loaded.
     config.paperclip_defaults = {
       :storage => :s3,
+      :s3_region => 'us-west-1',
       :s3_credentials => {
         :bucket => ENV["s3_bucket"],
         :access_key_id => ENV["s3_access_key_id"],
         :secret_access_key => ENV["s3_secret_access_key"],
-        :s3_region => ENV["s3_region"]
+        :s3_region => ENV["s3_region"],
       }
     }
   end

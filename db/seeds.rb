09619efb356dbfa94895570216023a7807befec9
mav-zate <maverick.alzate@gmail.com>
2017-08-25 16:55:25 -0400

    Fix database seeds for heroku push

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
 User.destroy_all
 u1 = User.create!(email: 'edcarl@gmail.com', first_name: 'Edcarl',
       last_name: 'Adraincem', password: 'password')
@@ -22,25 +23,26 @@ u4 = User.create!(email: 'bolei@gmail.com', first_name: 'Bolei',
 u5 = User.create!(email: 'mav@gmail.com', first_name: 'Maverick',
       last_name: 'Alzate', password: 'password')
 
+
 Bnb.destroy_all
-bnb1 = Bnb.create!(title: 'Penthouse condo', host_id: u1.id, lat: Faker::Address.unique.latitude,
-        lng: Faker::Address.unique.longitude, accomodation_count: 2, price: 63,
+bnb1 = Bnb.create!(title: 'Penthouse condo', host_id: u1.id, lat: 40.745152,
+        lng: -73.993909, accomodation_count: 2, price: 63,
         security_deposit: 19, cleaning_fee: 5, weekly_discount: 0.14,
         monthly_discount: 0.23, wifi: true, internet: true, kitchen: true,
         parking: false, essentials: false, pets_allowed: false,
         smoking_allowed: false, rating: 5, bed_count: 1, room_type: 'entire home',
         home_type: 'condo', image: File.open('app/assets/images/greek_villa.jpg'))
 
-bnb2 = Bnb.create!(title: 'Cozy apartment', host_id: u2.id, lat: Faker::Address.unique.latitude,
-        lng: Faker::Address.unique.longitude,
+bnb2 = Bnb.create!(title: 'Cozy apartment', host_id: u2.id, lat: 31.993618,
+        lng: 35.841555,
         accomodation_count: 3, price: 54, security_deposit: 23,
         cleaning_fee: 43, weekly_discount: 0.43, monthly_discount: 0.74, wifi: true,
         internet: true, kitchen: true, parking: true, essentials: true, pets_allowed: true,
         smoking_allowed: true, rating: 3, bed_count: 2, room_type: 'private room',
         home_type: 'apartment', image: File.open('app/assets/images/lakeside_apartment.jpg'))
 
-bnb3 = Bnb.create!(title: 'Seaside Villa', host_id: u3.id, lat: Faker::Address.unique.latitude,
-        lng: Faker::Address.unique.longitude,
+bnb3 = Bnb.create!(title: 'Seaside Villa', host_id: u3.id, lat: 24.483404,
+        lng: 54.360520,
         accomodation_count: 5, price: 345, security_deposit: 32,
         cleaning_fee: 32, weekly_discount: 0.23, monthly_discount: 0.34,
         wifi: true, internet: true, kitchen: true, parking: true,
@@ -48,16 +50,16 @@ bnb3 = Bnb.create!(title: 'Seaside Villa', host_id: u3.id, lat: Faker::Address.u
         rating: 4, bed_count: 3, room_type: 'private room',
         home_type: 'Villa', image: File.open('app/assets/images/moroccan_home.jpg'))
 
-bnb4 = Bnb.create!(title: 'Cozy cottage', host_id: u4.id, lat: Faker::Address.unique.latitude,
-        lng: Faker::Address.unique.longitude,
+bnb4 = Bnb.create!(title: 'Cozy cottage', host_id: u4.id, lat: 31.792852,
+        lng: 35.245915,
         accomodation_count: 45, price: 45, security_deposit: 45,
         cleaning_fee: 45, weekly_discount: 0.45, monthly_discount: 0.45, wifi: true,
         internet: true, kitchen: true, parking: true, essentials: true, pets_allowed: true,
         smoking_allowed: true, rating: 2, bed_count: 5, room_type: 'shared room',
         home_type: 'cottage', image: File.open('app/assets/images/quiet_yurt.jpg'))
 
-bnb5 = Bnb.create!(title: 'Rustic ranch', host_id: u5.id, lat: Faker::Address.unique.latitude,
-        lng: Faker::Address.unique.longitude,
+bnb5 = Bnb.create!(title: 'Rustic ranch', host_id: u5.id, lat: 40.710716,
+        lng: -73.819509,
         accomodation_count: 23, price: 23, security_deposit: 23,
         cleaning_fee: 23, weekly_discount: 0.23, monthly_discount: 0.23, wifi: true,
         internet: true, kitchen: true, parking: true, essentials: true, pets_allowed: true,

be05167f7c7f0319358f4e4f8171f439664b34bf
mav-zate <maverick.alzate@gmail.com>
2017-08-26 20:41:13 -0400

    Add map component to search page

--- frontend/components/map/bnb_map.jsx
+++ frontend/components/map/bnb_map.jsx
@@ -2,11 +2,19 @@ import React from 'react';
 
 
 class BnbMap extends React.Component {
+
+  componentDidMount() {
+    const mapOptions = {
+      center: { lat: 40.745234, lng: -73.994006 },
+      zoom: 16,
+    };
+
+    this.map = new google.maps.Map(this.mapNode, mapOptions);
+  }
+
   render() {
     return (
-      <div id="map-container" ref="map">
-        
-      </div>
+      <div id="map-container" ref={ map => this.mapNode = map } />
     );
   }
 }

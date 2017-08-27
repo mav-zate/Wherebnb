91fe2a5c8cb09cd258443ead9abc70ad52c763e2
mav-zate <maverick.alzate@gmail.com>
2017-08-27 11:16:05 -0400

    Add custom markers to show price of individual bnb

--- frontend/util/marker_manager.js
+++ frontend/util/marker_manager.js
@@ -1,3 +1,14 @@
+var MARKER_ICON = {
+    path: 'M 0,0 45,0 45,25 28,25 22.5,30 17,25 0,25 z',
+    strokeColor: '#484848',
+    fillColor: '#FFF',
+    fillOpacity: 1,
+    anchor: new google.maps.Point(22, 25),
+    origin: new google.maps.Point(0, 0),
+    size: new google.maps.Size(45, 25),
+    labelOrigin: new google.maps.Point(22, 13),
+  };
+
 export default class MarkerManager {
   constructor(map) {
     this.map = map;
@@ -17,6 +28,11 @@ export default class MarkerManager {
     this.markers[bnb.id] = new google.maps.Marker({
       position: { lat: bnb.lat, lng: bnb.lng },
       map: this.map,
+      label: {
+        text: `$ ${Math.floor(bnb.price)}`,
+        fontSize: '15px',
+      },
+      icon: MARKER_ICON,
     });
   }
 }

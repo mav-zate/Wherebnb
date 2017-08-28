a40182600ec1a7f09aa4eb998d24d10770108dd7
mav-zate <maverick.alzate@gmail.com>
2017-08-28 17:32:45 -0400

    Add links on bnb index list items to respective show pages

--- frontend/components/bnbs/index/bnb_index_item.jsx
+++ frontend/components/bnbs/index/bnb_index_item.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { renderStars, calculateRating } from '../star_render_methods';
+import { Link } from 'react-router-dom';
 
 class BnbIndexItem extends React.Component {
   constructor(props) {
@@ -17,26 +18,27 @@ class BnbIndexItem extends React.Component {
   render() {
     return (
       <li className="indexItem">
-        <div>
-          <span>
-            <img className="indexItem-image" src={this.props.bnb.image_url} />
-          </span>
-        </div>
-        <div className="indexItem-topRow">
-          <span>From</span>
-          <span>{`$${Math.floor(this.props.bnb.price)}`}</span>
-          <span>·</span>
-          <span>{this.props.bnb.title}</span>
-        </div>
-        <div className="indexItem-midRow">
-          <span>{this.props.bnb.room_type}</span>
-          <span>·</span>
-          <span>{this.renderBeds(this.props.bnb.bed_count)}</span>
-        </div>
-        <div className="indexItem-bottomRow">
-          {this.renderStars(this.props.bnb.reviews)}
-        </div>
-
+        <Link to={`/bnbs/${this.props.bnb.id}`}>
+          <div>
+            <span>
+              <img className="indexItem-image" src={this.props.bnb.image_url} />
+            </span>
+          </div>
+          <div className="indexItem-topRow">
+            <span>From</span>
+            <span>{`$${Math.floor(this.props.bnb.price)}`}</span>
+            <span>·</span>
+            <span>{this.props.bnb.title}</span>
+          </div>
+          <div className="indexItem-midRow">
+            <span>{this.props.bnb.room_type}</span>
+            <span>·</span>
+            <span>{this.renderBeds(this.props.bnb.bed_count)}</span>
+          </div>
+          <div className="indexItem-bottomRow">
+            {this.renderStars(this.props.bnb.reviews)}
+          </div>
+        </Link>
       </li>
   );
   }
